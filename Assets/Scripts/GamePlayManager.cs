879a72a0a1b39183d9366a987c5f727c2b605f63
TheMetabug <metabug@gmail.com>
2019-08-12 15:33:40 +0300

    Sample scene modified. Added Gameplaymanager, player object and "stage" to play around with.

---
+++ Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This GamePlayManager handles target framerate, scene changes and other
+ * "backend" stuff what affects the game itself and not gameobjects.
+ */
+public class GamePlayManager : MonoBehaviour
+{
+    void Start()
+    {
+        // TODO: target framerate is 30 by default now. Increase it to 60
+        // after game has more meat around bones to tweak stuff better.
+        Application.targetFrameRate = 30;
+    }
+
+    void Update()
+    {
+        // Restart scene by pressing F5 DEBUG PURPOSES ONLY
+        // TODO: lock this behind debug mode or just delete it
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
